71fc0dbc7e302104d5867717838c2570e39816dd
SophieVDev <webdesigner@sitify.fr>
2023-08-16 12:04:11 +0200

    creation about-us page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { AboutUsComponent} from './pages/about-us/about-us.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path:'',component:HomepageComponent },
+  {path: 'nous-connaître', component: AboutUsComponent},
 ];
 
 @NgModule({

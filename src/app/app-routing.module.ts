6fe92c6bf9578b4393b2f7ef4c3ab4a422009f4a
SophieVDev <webdesigner@sitify.fr>
2023-08-16 12:15:48 +0200

    creation mentions legaes page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { AboutUsComponent} from './pages/about-us/about-us.component';
 import { ContactUsComponent} from './pages/contact-us/contact-us.component';
+import { MentionsLegalesComponent} from './pages/mentions-legales/mentions-legales.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -8,6 +9,7 @@ const routes: Routes = [
   {path:'',component:HomepageComponent },
   {path: 'nous-connaître', component: AboutUsComponent},
   {path: 'nous-contacter', component: ContactUsComponent},
+  {path: 'mentions-legales', component: MentionsLegalesComponent},
 ];
 
 @NgModule({

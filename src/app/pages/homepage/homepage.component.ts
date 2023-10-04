d266b400741d20318633c8fe8e267445091eac7b
SophieVDev <webdesigner@sitify.fr>
2023-10-04 12:52:27 +0200

    Gestion du cookie alert

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -11,7 +11,7 @@ import { RgpdService } from '../../components/rgpd-alert-component/rgpdService';
 export class HomepageComponent implements OnInit{
   products: Products[] = [];
   produitsEnPromotion: Products[] = [];
-  hasRgpdConsent: boolean = true;
+  hasRgpdConsent: boolean = false;
 
   constructor(
     private homepageService: HomepageService,
@@ -19,12 +19,26 @@ export class HomepageComponent implements OnInit{
   ) {}
 
   ngOnInit() {
+    // Vérifiez si l'utilisateur a donné son consentement en utilisant localStorage
+    this.hasRgpdConsent = localStorage.getItem('rgpdConsent') === 'true';
+
+    // Affichez la boîte de dialogue des cookies uniquement si l'utilisateur n'a pas encore donné son consentement
+    if (!this.hasRgpdConsent) {
+      this.showCookieAlert();
+    }
+
+    // Chargez les données des produits
+    this.fetchData();
+  }
+
+  /*ngOnInit() {
     this.hasRgpdConsent = this.rgpdService.hasConsent();
 
     if (this.hasRgpdConsent) {
       this.fetchData();
     }
-  }
+  }*/
+
 
   private fetchData() {
     this.homepageService.getProducts().subscribe(
@@ -40,49 +54,35 @@ export class HomepageComponent implements OnInit{
     );
   }
 
+  private showCookieAlert() {
+    // Affichez la boîte de dialogue des cookies
+    // ...
+  }
+
   giveRgpdConsent() {
+    // Donnez le consentement et enregistrez-le dans localStorage
+    this.rgpdService.giveConsent();
+    localStorage.setItem('rgpdConsent', 'true');
+    this.hasRgpdConsent = true;
+  }
+
+ /* giveRgpdConsent() {
     this.rgpdService.giveConsent();
     this.hasRgpdConsent = true;
-    this.fetchData();
   }
 
-  //29/09/23/////////////
- /* products: Products[] = [];
-  produitsEnPromotion: Products[] = [];
-  
+  removeRgpdCookie() {
+    // Supprimez le cookie RGPD (code pour supprimer le cookie ici)
 
-  constructor(private homepageService: HomepageService) {}
+    // Masquez la boîte de dialogue des cookies
+    this.hasRgpdConsent = true;
+  } */
 
-  ngOnInit() {
-    this.fetchData();
-  }
-
-  private fetchData() {
-    this.homepageService.getProducts().subscribe(
-      (response) => {
-        this.products = response;
-
-        // Filtrer les produits en promotion une fois les données récupérées
-        this.produitsEnPromotion = this.products.filter(product => product.promotion > 0);
-      },
-      (error) => {
-        console.error("Erreur lors de la requête de produits :", error);
-      }
-    );*/
   }
 
 
 
 
-
-
-
-
-
-
-
-
- 
 
 
 
